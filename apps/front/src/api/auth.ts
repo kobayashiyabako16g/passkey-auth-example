e98ed9139bd4a5fc56582772692779e0ccd88a26
kobayashiyabako16g <all.laisser.fair@gmail.com>
2025-08-17 15:05:14 +0900

    Add: api route

--- apps/front/src/api/auth.ts
+++ apps/front/src/api/auth.ts
@@ -12,7 +12,7 @@ export const signUp = async (username: string) => {
   });
 
   if (!response.ok) {
-    throw new Error("Failed to sign in");
+    throw new Error("Failed to sign up");
   }
 
   const data = await response.json();
@@ -22,6 +22,44 @@ export const signUp = async (username: string) => {
 
 export const signUpFinish = async (dto: RegistrationResponse) => {
   const response = await fetch(`${API_URL}/passkey/register/finish`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dto),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to sign up");
+  }
+
+  const data = await response.json();
+
+  return data;
+};
+
+export const loginStart = async (username: string) => {
+  const response = await fetch(`${API_URL}/passkey/login/start`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to sign in");
+  }
+
+  const data = await response.json();
+
+  return data;
+};
+
+export const loginFinish = async (dto: RegistrationResponse) => {
+  const response = await fetch(`${API_URL}/passkey/login/finish`, {
     method: "POST",
     credentials: "include",
     headers: {
